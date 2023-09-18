d99970627b993e1cc4406ede16e97d25e500afd5
hungpv <phanhung2702@gmail.com>
2023-09-18 13:40:30 +0700

    [Add] sun move from bot to top

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className='barier'></div>
+      <div className="sun"></div>
+      {/* <div className="transition"></div> */}
+      <div className="top">
+        <section className="horizon-line horizon-line-top">
+          <div className="horizon-left"></div>
+          <div className="horizon-right"></div>
+        </section>
+      </div>
+      <div className="bot">
+        <section className="horizon-line horizon-line-bot">
+          <div className="horizon-left"></div>
+          <div className="horizon-right"></div>
+        </section>
+      </div>
     </div>
   );
 }
